313faac914e6e27db601f727c69d73c50b0f9cc2
Ryan Odd <ryan@stageten.tv>
2021-07-24 11:33:04 -0400

    Handle Resizing & place over game log

--- content-script.js
+++ content-script.js
@@ -1,75 +1,125 @@
 
 function attemptReadLog() {
-  let logSearchResults = document.getElementsByClassName('game-log')
+  let toggleButton = document.querySelector('#dt-toggleButton')
+
+  let logSearchResults = document.getElementsByClassName('log-container')
   if (logSearchResults.length === 1) {
-    wholeLog = logSearchResults[0].innerText
-    let iframe = document.querySelector('#dt-iframe')
-    iframe.contentWindow.postMessage(wholeLog, '*')
+    logContainerNode = logSearchResults[0]
+    
+    if (toggleButton.style.display === 'inline') {
+      let iframe = document.querySelector('#dt-iframe')
+      wholeLog = logContainerNode.innerText
+      iframe.contentWindow.postMessage(wholeLog, '*')
+    }
+    else {
+      onGameStartDetect()
+    }
   }
+  else {
+    if (toggleButton.style.display === 'inline'){
+      onGameFinishDetect()
+    }
+  }
+}
+
+function onGameStartDetect() {
+  if (setOverlayPlacement()) {
+    setOverlayVisible(true)
+  }
+}
+
+function setOverlayPlacement() {
+  let logSearchResults = document.getElementsByClassName('log-container')
+  let tabContainerSearchResults = document.getElementsByClassName('tab-container')
+  if (logSearchResults.length === 1 && tabContainerSearchResults.length === 1) {
+    logContainerNode = logSearchResults[0]
+    tabContainerNode = tabContainerSearchResults[0]
+    let toggleButton = document.querySelector('#dt-toggleButton')
+    let iframe = document.querySelector('#dt-iframe')
+
+    toggleButton.style.top = `${tabContainerNode.getBoundingClientRect().top + 2}px`// 2px border
+    toggleButton.style.left = `${tabContainerNode.getBoundingClientRect().left}px`
+    toggleButton.style.bottom = '0px'
+    iframe.style.top = `${logContainerNode.getBoundingClientRect().top + 2}px` // 2px border
+    iframe.style.left = `${logContainerNode.getBoundingClientRect().left}px`
+    iframe.style.height = `${logContainerNode.getBoundingClientRect().height - 2}px` // 2px border
+    iframe.style.width = `${logContainerNode.getBoundingClientRect().width}px`
+    
+    return true
+  }
+  return false
+}
+
+function setOverlayVisible(visible) {
+  let toggleButton = document.querySelector('#dt-toggleButton')
+  toggleButton.style.display = visible ? 'inline' : 'none'
+  let iframe = document.querySelector('#dt-iframe')
+  iframe.style.display = 'none'
+}
+
+function onGameFinishDetect() {
+  setOverlayVisible(false)
 }
 
 function watchLog() {
   attemptReadLog()
-  let observer = new MutationObserver(mutations => {
+  let logObserver = new MutationObserver(mutations => {
     for(let mutation of mutations) {
+      // This DOM-change watcher needs to cover all possible things to watch:
+      // - A line being added to the log
+      // - The entire log disappearing (game end)
+      // - The entire log appearing (game start)
       if (
-        mutation.target.className === 'log-line' &&
-        mutation.addedNodes.length >= 1
-      ) {
+        mutation.target.className === 'log-line' ||
+        mutation.target.hasAttribute('ng-if')) {
         attemptReadLog()
       }
     }
   });
-  observer.observe(document, { childList: true, subtree: true });
+  logObserver.observe(document, { childList: true, subtree: true });
+
+  let resizeObserver = new MutationObserver(mutations => {
+    for(let mutation of mutations) {
+      // This DOM-change watcher needs to cover all possible things to watch:
+      // - The chat container being minimized/shown
+      if (mutation.target.className.split(' ').includes('chat-container')){
+        setTimeout(setOverlayPlacement, 400)
+      }
+    }
+  });
+  resizeObserver.observe(document, { childList: true, subtree: true, attributes: true });
+  window.addEventListener('resize', setOverlayPlacement);
 }
 
 function injectOverlay() {
-  const containerDivStyle = '\
-    position: absolute;\
-    left: 5px;\
-    bottom: 5px;\
-    z-index: 100;\
-  ';
   let containerDiv = document.createElement('div');
   containerDiv.id = 'dt-container'
-  containerDiv.style = containerDivStyle
+  containerDiv.style.position = 'absolute'
+  containerDiv.style.zIndex = '100'
   document.querySelector('body').append(containerDiv);
   
-  const iframeStyle = '\
-    width: calc(100vw - 10px);\
-    height: 600px;\
-    border: 0;\
-  ';
   let iframe = document.createElement('iframe');
   iframe.title = 'Dominion Tracker Overlay'
-  iframe.src = 'https://dominiontracker.com/#/overlay';
-  // iframe.src = 'http://localhost:3000/#/overlay';
+  // iframe.src = 'https://dominiontracker.com/#/overlay';
+  iframe.src = 'http://localhost:3000/#/overlay';
   iframe.id = 'dt-iframe'
-  iframe.style = iframeStyle + 'display: none;'
+  iframe.style.display = 'none'
+  iframe.style.position = 'fixed'
+  iframe.style.width = '100%' // without this, it's capped at 300px for some reason
   document.querySelector('#dt-container').append(iframe);
   iframe.contentWindow.console = console;
 
-  const buttonStyle = '\
-    display: block;\
-    padding: 8px 12px;\
-    background-color: #ff0000;\
-    color: white;\
-    text-decoration: none;\
-    border: none;\
-    border-radius: 4px;\
-  '
   let toggleButton = document.createElement('button');
-  toggleButton.textContent = 'DECK'
-  toggleButton.style = buttonStyle
+  toggleButton.id = 'dt-toggleButton' 
+  toggleButton.textContent = 'TRACKER'
+  toggleButton.style.display = 'inline'
+  toggleButton.style.position = 'fixed'
+  toggleButton.style.backgroundColor = '#ff0000'
+  toggleButton.style.color = 'white'
+  toggleButton.style.textDecoration = 'none'
+  toggleButton.style.border = 'none'
   toggleButton.onclick = () => {
     let detectedIframe = document.querySelector('#dt-iframe')
-
-    // silly way of storing state in the DOM; watchLog should only call once
-    if (detectedIframe.style.zIndex !== '69') {
-      detectedIframe.style.zIndex = '69'
-      watchLog()
-    }
-
     let isTrackerOpen = detectedIframe.style.display === 'inline'
     detectedIframe.style.display = isTrackerOpen ? 'none' : 'inline'
   }
@@ -78,6 +128,7 @@ function injectOverlay() {
 
 try {
   injectOverlay()
+  watchLog()
 } catch (error) {
   console.error('Content-script error:', error)
 }
